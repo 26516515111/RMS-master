7bc7bbe1c4cdb92879c56bc6afa5da336179c8cc
26516515111 <169065111+26516515111@users.noreply.github.com>
2024-07-12 16:14:26 +0800

    Update main.cpp

--- cpp-qt-manageSystem-master/main.cpp
+++ cpp-qt-manageSystem-master/main.cpp
@@ -1,12 +1,17 @@
 #include "mainwindow.h"
 #include"logindia.h"
 #include <QApplication>
+#include"database.h"
 
 //#define IS_DEBUG
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    if(!Database::createConnection()){
+        return 1;
+    }
+
     MainWindow w;
 #ifndef IS_DEBUG
     LoginDia logDia;
